10820d1daf2fa5e40abac29c378cada3cf1045f0
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-11-15 14:35:30 +0100

    ticketing-project-spring security

--- ticketing-project-security_SelfPractice/src/main/java/com/cydeo/config/SecurityConfig.java
+++ ticketing-project-security_SelfPractice/src/main/java/com/cydeo/config/SecurityConfig.java
@@ -2,12 +2,14 @@ package com.cydeo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,7 +18,7 @@ import java.util.List;
 @Configuration
 public class SecurityConfig {
 
-    @Bean  //manually , no have connect to DB yet
+    @Bean  //manually , no have connect to DB yet, here we override my User to Spring Security User
     public UserDetailsService userDetailsService(PasswordEncoder encoder){
         //PasswordEncoder encoder to create encoder password from my password which is coming from login page
 
@@ -28,6 +30,27 @@ public class SecurityConfig {
 
         return new InMemoryUserDetailsManager(userList); // it says where to save this userList
 
+        //we passed Spring Security's User to UI-Part with my user information manually
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .authorizeRequests() //we need to authorize pages, who will see, who should not see
+                .antMatchers(
+                        "/",
+                        "/login",
+                        "/fragments/**",
+                        "/assets/**",
+                        "/images/**"
+
+                ).permitAll() // those above pages are permitted for all
+                .anyRequest().authenticated() // other pages are needed to be authenticated
+                .and()
+                .httpBasic() //popup box
+                .and().build();
 
     }
 
